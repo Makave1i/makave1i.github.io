4ac6592064d5f8db1dbf464cf9609b13912cca4a
Oleksandr Chuhunov <oleksandr.chuhunov@horizonoasis.ae>
2026-02-16 18:59:40 +0200

    hills

--- assets/browserAll-BH2bDM7H.js
+++
@@ -1,2 +0,0 @@
-import{e as a,A as e,C as i,a as s,E as t,F as n,D as d}from"./pixi-BagO6QuG.js";import"./webworkerAll-DYevsdjx.js";a.add(e);a.mixin(i,s);a.add(t);a.mixin(i,n);a.add(d);
-//# sourceMappingURL=browserAll-BH2bDM7H.js.map
