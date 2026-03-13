6d9675880c23715473b234ecec0cd908cf8eae71
Oleksandr Chuhunov <oleksandr.chuhunov@horizonoasis.ae>
2026-03-13 14:21:39 +0200

    v 0.2.2.5

---
+++ assets/browserAll-BCOGx7Vp.js
@@ -0,0 +1,2 @@
+import{e as a,A as e,C as i,a as s,E as t,F as n,D as d}from"./pixi-Da9crsSu.js";import"./webworkerAll-C_y-SVcY.js";a.add(e);a.mixin(i,s);a.add(t);a.mixin(i,n);a.add(d);
+//# sourceMappingURL=browserAll-BCOGx7Vp.js.map
--- assets/webworkerAll-OdJCc-8M.js
+++
@@ -1,2 +0,0 @@
-import{e as a,q as e,r as d,s as i,t as s,M as t,u as p,v as r,w as P,x as n,y as l,H as T,z as x,I as m,J as S,K as c,L as o}from"./pixi-C2TOLXgO.js";a.add(e);a.add(d);a.add(i);a.add(s);a.add(t);a.add(p);a.add(r);a.add(P);a.add(n);a.add(l);a.add(T);a.add(x);a.add(m);a.add(S);a.add(c);a.add(o);
-//# sourceMappingURL=webworkerAll-OdJCc-8M.js.map
